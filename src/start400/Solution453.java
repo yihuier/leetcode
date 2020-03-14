f3d32dd4928e2ddf728100dca68ff535e03d1601
yihuier <yihui_mail@foxmail.com>
2020-03-14 15:32:45 +0800

    有的问题，很难从正面去思考，可以尝试换个角度，反过来思考。

---
+++ src/start400/Solution453.java
@@ -0,0 +1,24 @@
+package start400;
+
+public class Solution453 {
+
+    /**
+     * 将问题转换为每移动一次，有一个元素减1，所以只要所有元素减到和最小值一样就可以了
+     * 下面的做法是两次循环，也可以在一次循环中统计最小值个数以及所有元素的和
+     * 最后在减去最小值的和。
+     */
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int moves = 0;
+
+        for (int num : nums) {
+            if (num < min) min = num;
+        }
+
+        for (int num : nums) {
+            if (num > min) moves += num - min;
+        }
+
+        return moves;
+    }
+}
