208cb5913df1666709086f27cc57b872a62a02e0
yihuier <yihui_mail@foxmail.com>
2020-03-18 11:42:16 +0800

    509、不在状态

---
+++ src/start500/Solution509.java
@@ -0,0 +1,27 @@
+package start500;
+
+public class Solution509 {
+
+    /**
+     * 非递归，斐波那契
+     */
+    public int fib(int N) {
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+
+        int result = 0;
+        int lastFirst = 0;
+        int lastSecond = 1;
+        int i = 2;
+
+       while (i <= N) {
+           result = lastFirst + lastSecond;
+           lastFirst = lastSecond;
+           lastSecond = result;
+
+           i++;
+       }
+
+       return result;
+    }
+}
