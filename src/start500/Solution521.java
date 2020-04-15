db8b27c14a0e1d9ee680b5e2b04ee54c80fb97a3
yihuier <yihui_mail@foxmail.com>
2020-04-15 21:44:40 +0800

    521

---
+++ src/start500/Solution521.java
@@ -0,0 +1,17 @@
+package start500;
+
+/**
+ *  @author yihuier
+ *  @Date 2020/4/15 21:02
+ *  @Description
+ */
+public class Solution521 {
+
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+
+        return Math.max(a.length(), b.length());
+    }
+}
