c340c27013a4fde01cf6a88f3e19eb0c04769b5a
yihuier <yihui_mail@foxmail.com>
2020-04-16 20:51:06 +0800

    530、另一种效率高的做法，看不懂啊！！！

---
+++ src/start500/Solution530.java
@@ -0,0 +1,43 @@
+package start500;
+
+import util.TreeNode;
+
+/**
+ *  @author yihuier
+ *  @Date 2020/4/16 20:14
+ *  @Description 只会这种效率低的做法，另一种暂时不能理解
+ */
+public class Solution530 {
+
+    public int getMinimumDifference(TreeNode root) {
+        int[] min = new int[1];
+        min[0] = Integer.MAX_VALUE;
+
+        findMinDifOfRoot(root, min);
+
+        return min[0];
+    }
+
+    private void findMinDifOfRoot(TreeNode root, int[] min) {
+        if (root == null) {
+            return;
+        }
+
+        getMinDif(root, root.left, min);
+        getMinDif(root, root.right, min);
+
+        findMinDifOfRoot(root.left, min);
+        findMinDifOfRoot(root.right, min);
+    }
+
+    private void getMinDif(TreeNode root, TreeNode current, int[] min) {
+        if (current == null) {
+            return;
+        }
+
+        min[0] = Math.min(min[0], Math.abs(root.val - current.val));
+
+        getMinDif(root, current.left, min);
+        getMinDif(root, current.right, min);
+    }
+}
