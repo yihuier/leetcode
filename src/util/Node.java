4a57ebd6aad990592b98f1b40525a451bf71cdb2
yihuier <yihui_mail@foxmail.com>
2020-09-11 17:18:26 +0800

    链表练习-day2

---
+++ src/util/Node.java
@@ -0,0 +1,13 @@
+package util;
+
+/**
+ *  @author yihuier
+ *  @Date 2020/9/11 13:26
+ *  @Description
+ */
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+}
